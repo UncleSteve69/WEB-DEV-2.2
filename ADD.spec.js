7f9c8a4ecd77b73531bbbfa2804deb4b3bb5820e
UncleSteve69 <61479356+UncleSteve69@users.noreply.github.com>
2020-06-11 23:02:37 +0300

    Update and rename Register.spec.js to ADD.spec.js

--- ADD.spec.js
+++ ADD.spec.js
@@ -5,6 +5,6 @@ import Cocktail from "../../src/views/Cocktail";
 describe('TheatricalDetail.vue', () => {
      it('renders renders button tag', () => {
         const wrapper = shallowMount(Cocktail);
-        expect(wrapper.find('button').text()).equal('')
+        expect(wrapper.find('button').text()).equal('ADD')
     })
 });
